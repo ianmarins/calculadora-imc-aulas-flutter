03353320957f8d013a1b8548a9a724ae0c26c678
ianmarins <ianmarins@gmail.com>
2019-09-07 22:08:43 -0300

    Alteraões e adiçoes dos intems 1/2/4

--- lib/main.dart
+++ lib/main.dart
@@ -17,19 +17,32 @@ class _HomeState extends State<Home> {
 
   TextEditingController _weightController = TextEditingController();
   TextEditingController _heightController = TextEditingController();
-  String _result;
+    
+    String _result;
+    double _imc = 0;
+    int _selectedRadio;
 
   @override
   void initState() {
     super.initState();
     resetFields();
+    _selectedRadio = 0;
   }
 
   void resetFields() {
     _weightController.text = '';
     _heightController.text = '';
+     _selectedRadio = null;
+     _imc = 0;
     setState(() {
-      _result = 'Informe seus dados';
+      _result = 'Informe os dados';
+    });
+  }
+
+
+   void _setSelectRadio(int value){
+    setState(() {
+     _selectedRadio = value; 
     });
   }
 
@@ -56,6 +69,39 @@ class _HomeState extends State<Home> {
       ],
     );
   }
+    
+
+   Widget _buildRadioButtom(){
+    return new Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        new Text("Masculino",
+        style: new TextStyle(
+          fontSize: 15.0,
+        )),
+        new Radio(
+          value: 1,
+          groupValue: _selectedRadio,
+          onChanged: (value) {
+            print("Radio $value");
+            _setSelectRadio(value);
+          },
+        ),
+        new Text("Feminino",
+        style: new TextStyle(
+          fontSize: 15.0,
+        )),
+        new Radio(
+          value: 2,
+          groupValue: _selectedRadio,
+          onChanged: (value) {
+            print("Radio $value");
+            _setSelectRadio(value);
+          },
+        ),
+      ],
+    );
+  }
 
   Form buildForm() {
     return Form(
@@ -71,6 +117,7 @@ class _HomeState extends State<Home> {
               label: "Altura (cm)",
               error: "Insira uma altura!",
               controller: _heightController),
+          _buildRadioButtom(),
           buildTextResult(),
           buildCalculateButton(),
         ],
@@ -78,27 +125,46 @@ class _HomeState extends State<Home> {
     );
   }
 
+  //! FUNCAO CALCULAR IMC HOMEM/MULHER
+
   void calculateImc() {
     double weight = double.parse(_weightController.text);
     double height = double.parse(_heightController.text) / 100.0;
-    double imc = weight / (height * height);
+    _imc = weight / (height * height);
 
-    setState(() {
-      _result = "IMC = ${imc.toStringAsPrecision(2)}\n";
-      if (imc < 18.6)
-        _result += "Abaixo do peso";
-      else if (imc < 25.0)
-        _result += "Peso ideal";
-      else if (imc < 30.0)
-        _result += "Levemente acima do peso";
-      else if (imc < 35.0)
-        _result += "Obesidade Grau I";
-      else if (imc < 40.0)
-        _result += "Obesidade Grau II";
-      else
-        _result += "Obesidade Grau IIII";
-    });
-  }
+    if(_selectedRadio == 1){  //CALCULO PARA MASCULINO
+        setState(() {
+            _result = "IMC = ${_imc.toStringAsPrecision(3)}\n";
+            if (_imc < 20.7)
+              _result += "Abaixo do peso";
+            else if (_imc < 26.4)
+              _result += "Peso Normal Ideal";
+            else if (_imc < 27.8)
+              _result += "Sobrepeso";
+            else if (_imc < 31.1)
+              _result += "Obesidade Grau 1";
+            else
+              _result += "Obesidade Grau 2";
+          });
+          } else if(_selectedRadio == 2){
+              setState(() {
+            _result = "IMC = ${_imc.toStringAsPrecision(3)}\n";
+            if (_imc < 19.1)
+            _result += "Abaixo do peso"; //
+            else if (_imc < 25.8)
+              _result += "Peso Normal Ideal";
+            else if (_imc < 27.3)
+              _result += "Sobrepeso";
+            else if (_imc < 32.3)
+              _result += "Obesidade Grau 1";
+            else
+              _result += "Obesidade Grau 2";
+          });
+          }
+    }
+
+ 
+  
 
   Widget buildCalculateButton() {
     return Padding(
@@ -114,6 +180,8 @@ class _HomeState extends State<Home> {
     );
   }
 
+/*
+
   Widget buildTextResult() {
     return Padding(
       padding: EdgeInsets.symmetric(vertical: 36.0),
@@ -122,7 +190,65 @@ class _HomeState extends State<Home> {
         textAlign: TextAlign.center,
       ),
     );
+  }  // ATUAL
+
+
+  */
+
+
+
+      Widget buildTextResult() {
+    //int tempSelectedRadio = 0;
+    if (_selectedRadio == 0 || _selectedRadio == null && _imc == 0){
+      return buildPaddingColors(Colors.lightBlue);
+    }
+    else if (_selectedRadio == 1){
+    if (_imc == 0)
+    return buildPaddingColors(Colors.black);
+    else if (_imc < 20.7)
+    return buildPaddingColors(Colors.blue);
+    else if (_imc < 26.4)
+    return buildPaddingColors(Colors.green);
+    else if (_imc < 27.8)
+    return buildPaddingColors(Colors.yellow[900]);
+    else if (_imc < 31.1)
+    return buildPaddingColors(Colors.orange[900]);
+    else
+    return buildPaddingColors(Colors.redAccent[700]);
+    }
+    else if (_selectedRadio == 2){
+    if (_imc == 0)
+    return buildPaddingColors(Colors.black);
+    else if (_imc < 19.1)
+    return buildPaddingColors(Colors.blue);
+    else if (_imc < 25.8)
+    return buildPaddingColors(Colors.green);
+    else if (_imc < 27.3)
+    return buildPaddingColors(Colors.yellow[900]);
+    else if (_imc < 32.3)
+    return buildPaddingColors(Colors.orange[900]);
+    else
+    return buildPaddingColors(Colors.redAccent[700]);
+    }
   }
+
+
+  Widget buildPaddingColors(Color color) {
+            return Padding(
+            padding: EdgeInsets.symmetric(vertical: 36.0),
+            child: Text(
+              _result,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: color,
+                fontSize: 22.0,
+                fontWeight: FontWeight.bold, ),
+            ),
+          );
+        }
+      }
+
+
 
   Widget buildTextFormField(
       {TextEditingController controller, String error, String label}) {
@@ -135,4 +261,4 @@ class _HomeState extends State<Home> {
       },
     );
   }
-}
+
